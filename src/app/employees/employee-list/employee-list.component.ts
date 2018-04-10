93d4f951d2d1e5b2c41000328630f14ded387bbc
erdoeg <vmerdoeg@outlook.com>
2018-04-10 14:10:29 -0300

    Initial CRUD Angular Firebase

---
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -0,0 +1,40 @@
+import { Employee } from './../shared/employee.model';
+import { EmployeeService } from './../shared/employee.service';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  employeeList: Employee[];
+
+  constructor(private employeeService: EmployeeService, private toastr: ToastrService) { }
+
+  ngOnInit() {
+    var x = this.employeeService.getData();
+    x.snapshotChanges().subscribe(item => {
+      this.employeeList = [];
+      item.forEach(element => {
+        var y = element.payload.toJSON();
+        y["$key"] = element.key;
+        this.employeeList.push(y as Employee);
+      });
+    });
+  }
+
+  onEdit(emp: Employee) {
+    this.employeeService.selectedEmployee = Object.assign({}, emp);
+  }
+
+  onDelete(key: string) {
+    if (confirm('Do you want to delete this record?') == true) {
+      this.employeeService.deleteEmployee(key);
+      this.toastr.warning('Deleted Successfully', 'Employee Register');
+    }
+
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAHkUUt_k-yUq6Y_C-Lndhg895gswne3gQ",
+    authDomain: "angular5crud-12f83.firebaseapp.com",
+    databaseURL: "https://angular5crud-12f83.firebaseio.com",
+    projectId: "angular5crud-12f83",
+    storageBucket: "angular5crud-12f83.appspot.com",
+    messagingSenderId: "130966670841"
+  }
 };
